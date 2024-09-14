94b5a3c4e410b4182f8fbb49576c340eb28aa142
saljaoui <soufian2022ff@gmail.com>
2024-09-14 12:22:28 +0100

    update

--- test/test.go
+++ test/test.go
@@ -7,41 +7,39 @@ import (
 	"strings"
 )
 
-// Graph represents a graph using an adjacency list.
 type Graph struct {
 	adjacencyList map[string][]string
-
 }
-// NewGraph creates a new graph.
+
 func NewGraph() *Graph {
 	return &Graph{adjacencyList: make(map[string][]string)}
 }
 
-// AddEdge adds an edge to the graph.
+
 func (g *Graph) AddEdge(from, to string) {
 	g.adjacencyList[from] = append(g.adjacencyList[from], to)
-	g.adjacencyList[to] = append(g.adjacencyList[to], from) // For undirected graph
+	g.adjacencyList[to] = append(g.adjacencyList[to], from)
 }
 
-// BFS performs Breadth-First Search starting from the given start node to find the shortest path to end node.
+
 func (g *Graph) BFS(start, end string) []string {
 	visited := make(map[string]bool)
 	queue := []string{}
-	parent := make(map[string]string) // Maps node to its predecessor
+	parent := make(map[string]string) 
 	var paths []string
 
 
-	// Enqueue the start node and mark it as visited.
+
 	queue = append(queue, start)
 	visited[start] = true
-	parent[start] = "" // Start node has no parent
+	parent[start] = "" 
 
 	for len(queue) > 0 {
-		// Dequeue a node from the front of the queue.
+
 		node := queue[0]
 		queue = queue[1:]
 
-		// If we've reached the end node, reconstruct and return the path.
+
 		if node == end {
 		 path := reconstructPath(parent, end)
 		 
@@ -63,7 +61,7 @@ func (g *Graph) BFS(start, end string) []string {
 		 fmt.Println(path)
 		}
 
-		// Enqueue all unvisited neighbors.
+
 		for _, neighbor := range g.adjacencyList[node] {
 			if !visited[neighbor] {
 				queue = append(queue, neighbor)
@@ -73,20 +71,19 @@ func (g *Graph) BFS(start, end string) []string {
 		}
 	}
 
-	// Return an empty path if no path is found
+
 	return []string{}
 }
 
-// reconstructPath reconstructs the path from start to end using the parent map.
+
 func reconstructPath(parent map[string]string, end string) []string {
 	var path []string
 	for node := end; node != ""; node = parent[node] {
-		path = append([]string{node}, path...) // Prepend node to path
+		path = append([]string{node}, path...) 
 	}
 	return path
 }
 
-// AddRoom adds a new room to the graph with no edges.
 func (g *Graph) AddRoom(room string) {
 	if _, exists := g.adjacencyList[room]; !exists {
 		g.adjacencyList[room] = nil
@@ -94,7 +91,7 @@ func (g *Graph) AddRoom(room string) {
 }
 
 func main() {
-	// Create a new graph and add edges.
+
 	graph := NewGraph()
 	// edges := []string{"0-1", "0-3", "0-2", "2-5", "3-6", "1-4", "4-6"}
 
